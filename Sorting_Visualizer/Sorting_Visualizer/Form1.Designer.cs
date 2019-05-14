e5a62ab053480d978fc2d85f975c8eee2b41af72
Alex <alexmfv1@gmail.com>
2019-05-14 04:45:10 +0100

    Changed project to C#, added bars and randomize/sort buttons

---
+++ Sorting_Visualizer/Sorting_Visualizer/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Sorting_Visualizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.btnRandomize = new System.Windows.Forms.Button();
+            this.btnFastSort = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.mainPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.BackColor = System.Drawing.Color.LightGray;
+            this.mainPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.mainPanel.Controls.Add(this.btnFastSort);
+            this.mainPanel.Controls.Add(this.btnRandomize);
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.mainPanel.Location = new System.Drawing.Point(984, 0);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(280, 681);
+            this.mainPanel.TabIndex = 0;
+            // 
+            // btnRandomize
+            // 
+            this.btnRandomize.Location = new System.Drawing.Point(13, 629);
+            this.btnRandomize.Name = "btnRandomize";
+            this.btnRandomize.Size = new System.Drawing.Size(253, 40);
+            this.btnRandomize.TabIndex = 0;
+            this.btnRandomize.Text = "Randomize";
+            this.btnRandomize.UseVisualStyleBackColor = true;
+            this.btnRandomize.Click += new System.EventHandler(this.btnRandomize_Click);
+            // 
+            // btnFastSort
+            // 
+            this.btnFastSort.Location = new System.Drawing.Point(13, 583);
+            this.btnFastSort.Name = "btnFastSort";
+            this.btnFastSort.Size = new System.Drawing.Size(253, 40);
+            this.btnFastSort.TabIndex = 1;
+            this.btnFastSort.Text = "Instant Sort (No Algorithm)";
+            this.btnFastSort.UseVisualStyleBackColor = true;
+            this.btnFastSort.Click += new System.EventHandler(this.btnFastSort_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(984, 681);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.mainPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Sorting Visualizer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.mainPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Button btnFastSort;
+        private System.Windows.Forms.Button btnRandomize;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Sorting_Visualizer/Sorting_Visualizer/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sorting_Visualizer
+{
+    public partial class Form1 : Form
+    {
+        //Drawing Methods
+        Graphics gfx;
+        Brush myBrush;
+
+        //Variables
+        List<int> values = new List<int>();
+        int arraySize = 100;
+
+        //Rectangle Sizes
+        int RECT_WIDTH = 1;
+        //int RECT_SPACING = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            gfx = panel1.CreateGraphics();
+            myBrush = new SolidBrush(Color.White);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FillArray();
+            GetRectangleSize();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            for(int i = 0; i < values.Count(); i++)
+            {
+                Rectangle rect = new Rectangle(i * RECT_WIDTH + i, panel1.Height-values[i], RECT_WIDTH, values[i]);
+                gfx.FillRectangle(myBrush, rect);
+            }
+        }
+
+        //Fills the array with the initial values
+        public void FillArray()
+        {
+            //Clears the array if its not empty already
+            if(values.Count() > 0)
+                values.Clear();
+
+            for (int i = 6; i < arraySize * 6; i += 6)
+                values.Add(i);
+        }
+
+        //Randomizes the Array
+        public void RandomizeArray()
+        {
+            Random rand = new Random();
+            int valueStore = 0;
+            int idx = 0;
+
+            for (int i = 0; i < values.Count(); i++)
+            {
+                idx = rand.Next(0, values.Count());
+                valueStore = values[idx];
+                values[idx] = values[i];
+                values[i] = valueStore;
+            }
+        }
+
+        //Sorts array without using the proposed algorithm selection
+        public void FastSortArray()
+        {
+            values.Sort();
+        }
+
+        public void GetRectangleSize()
+        {
+            RECT_WIDTH = (int)(panel1.Width - values.Count()) / values.Count();
+        }
+
+        private void btnRandomize_Click(object sender, EventArgs e)
+        {
+            RandomizeArray();
+            this.Refresh();
+        }
+
+        private void btnFastSort_Click(object sender, EventArgs e)
+        {
+            FastSortArray();
+            this.Refresh();
+        }
+    }
+}
